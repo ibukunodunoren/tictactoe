8bdbba2843b0e302bfd23c77d9e69560cceec09a
ibukunodunoren <i.odunoren@hotmail.com>
2022-09-08 15:05:10 +0100

    commit fiur

---
+++ tictactoe/src/tictactoe/Game.java
@@ -0,0 +1,20 @@
+package tictactoe;
+
+import javax.swing.*;
+import java.awt.*;
+
+
+public class Game extends JFrame{
+	
+	public Game() {
+		this.add(new Graphics());
+		this.setTitle("Tic Tac Toe");
+		this.pack();
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setResizable(false);
+		this.setVisible(true);
+		this.setLocationRelativeTo(null);
+		
+	}
+	
+}
